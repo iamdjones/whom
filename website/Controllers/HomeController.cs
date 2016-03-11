cf6246b2937f5405f1a7448a3cfa6b10b6b413b9
Jones, Devon L (EED) <devon.jones@alaska.gov>
2016-03-11 12:09:30 -0900

    move business logic to website

--- website/Controllers/HomeController.cs
+++ website/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Microsoft.Exchange.WebServices.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Whom;
+using website.Models;
 
 namespace website.Controllers
 {
@@ -11,8 +12,58 @@ namespace website.Controllers
     {
         public ActionResult Index()
         {
-            var derp = Program.IndexViewModel();
-            return View(derp);
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+            service.Credentials = new WebCredentials("djones9898@hotmail.com", "Fatboyz69");
+            service.Url = new Uri(@"https://outlook.live.com/EWS/Exchange.asmx");
+
+            Folder inbox = Folder.Bind(service, WellKnownFolderName.Inbox);
+
+
+
+
+            var viewModel = new List<Sender>();
+
+            ItemView itemView = new ItemView(10000);
+            itemView.OrderBy.Add(EmailMessageSchema.DateTimeReceived, SortDirection.Descending);
+
+            Grouping g = new Grouping();
+
+            g.GroupOn = EmailMessageSchema.From;
+            g.AggregateOn = EmailMessageSchema.DateTimeReceived;
+            g.AggregateType = AggregateType.Minimum;
+            g.SortDirection = SortDirection.Descending;
+
+            SearchFilter.ContainsSubstring searchFilter = new SearchFilter.ContainsSubstring(EmailMessageSchema.From, "Nootrobox Club", ContainmentMode.Substring, ComparisonMode.IgnoreCaseAndNonSpacingCharacters);
+
+            var groups = inbox.FindItems(itemView, g);
+
+
+
+            Console.WriteLine("Found {0} groups", groups.Count());
+
+            foreach (var group in groups)
+            {
+                var email = group.Items.First() as EmailMessage;
+                //var firstItem = item.Items.First();
+                var items = group.Items;
+                //var groupName = (item.Items.First() as EmailMessage).Sender;
+                //var groupAggregate = (item.Items.First() as EmailMessage).DateTimeReceived;
+
+                //Console.WriteLine("{0} {1}", groupName, groupAggregate);
+                var messages = group.Items;
+                var sender = new Sender();
+                sender.name = email.Sender.Name;
+                sender.messages.AddRange(from m in messages select new Message() { subject = m.Subject, recieved = m.DateTimeReceived });
+
+                viewModel.Add(sender);
+
+                Console.WriteLine("{0} {1}", email.Sender, email.DateTimeReceived);
+                Console.WriteLine("\t{0}", email.Subject);
+
+            }
+
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ website/Models/ExchangeModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace website.Models
+{
+    public class Sender
+    {
+        public string name { get; internal set; }
+        public List<Message> messages { get; internal set; }
+
+        public string subject { get { return messages.First().subject; } }
+        public DateTime recieved { get { return messages.First().recieved; } }
+        public string body { get { return messages.First().body; } }
+
+        public Sender()
+        {
+            messages = new List<Message>();
+        }
+
+    }
+
+    public class Message
+    {
+        public string body { get; internal set; }
+        public DateTime recieved { get; internal set; }
+        public string subject { get; internal set; }
+
+    }
+}
